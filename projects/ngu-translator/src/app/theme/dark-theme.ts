2c43dd56fe018284ba189b436d9150c4f030716b
Sheik Althaf <althafmech@outlook.com>
2019-01-07 23:43:59 +0530

    theming added

---
+++ projects/ngu-translator/src/app/theme/dark-theme.ts
@@ -0,0 +1,17 @@
+import { Theme } from './symbols';
+
+export const darkTheme: Theme = {
+  name: 'dark',
+  properties: {
+    '--background': '#1F2125',
+    '--on-background': '#fff',
+    '--primary': '#2F90FF',
+    '--on-primary': '#fff',
+    '--secondary': '#474A4F',
+    '--on-secondary': '#fff',
+    '--surface': '#282A2F',
+    '--on-surface': '#dddede',
+    '--error': '#E74E3C',
+    '--on-error': '#fff'
+  }
+};
---
+++ projects/ngu-translator/src/app/theme/light-theme.ts
@@ -0,0 +1,17 @@
+import { Theme } from './symbols';
+
+export const lightTheme: Theme = {
+  name: 'light',
+  properties: {
+    '--background': '#f6f7f9',
+    '--on-background': '#000',
+    '--primary': '#1976d2',
+    '--on-primary': '#fff',
+    '--secondary': '#a8a8a8',
+    '--on-secondary': '#fff',
+    '--surface': '#fff',
+    '--on-surface': '#000',
+    '--error': '#E74E3C',
+    '--on-error': '#fff'
+  }
+};
